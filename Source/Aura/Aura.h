87a36ff9d409e1daa3c43559b94fe10f7200ee1a
KingdaGhost <bryankhongdeit@gmail.com>
2024-02-13 19:33:02 +0530

    Base Character class

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright KingdaGhost Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,30 @@
+// Copyright KingdaGhost Studios
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
