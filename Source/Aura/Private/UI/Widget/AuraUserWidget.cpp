52853d586dfd059c8ef4cf99ef86ef8d89b35536
KingdaGhost <bryankhongdeit@gmail.com>
2024-02-23 12:05:36 +0530

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright KingdaGhost Studios
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
