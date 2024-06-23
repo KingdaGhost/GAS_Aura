b018223ea58e279c2eb75584491a4d8aa85c60e5
Evangelene Bryan <evangelenebryan@gmail.com>
2024-06-23 22:18:18 +0530

    Aura Ability System Blueprint Function Library

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,21 @@
+// Copyright KingdaGhost Studios
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	// The meta tag is to eliminate the task of inputting self as the input for this pure function
+	UFUNCTION(BlueprintPure, Category="AuraAbilitySystemLibrary|WidgetController", meta=(WorldContext="WorldContextObject"))
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
