dc2cd521c82c476c17476ded9f50117cd4ab3e3a
Evangelene Bryan <evangelenebryan@gmail.com>
2024-07-02 11:45:44 +0530

    Spawn firebolt from event

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -12,8 +12,12 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
                                            const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+}
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
@@ -30,10 +34,9 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 			GetOwningActorFromActorInfo(),
 			Cast<APawn>(GetOwningActorFromActorInfo()),
 			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
-
+	
 		//TODO: Give the projectile a gameplay effect spec for causing damage.
 		
 		Projectile->FinishSpawning(SpawnTransform);
 	}
-
 }
