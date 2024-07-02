ac81d8fe219323e3e8e647921e9fdc9855ca524a
Evangelene Bryan <evangelenebryan@gmail.com>
2024-07-02 23:22:20 +0530

    Custom Ability Tasks

---
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -0,0 +1,21 @@
+// Copyright KingdaGhost Studios
+
+
+#include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	// We can set data here before returning
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	ValidData.Broadcast(CursorHit.Location);
+}
+
+
