e2b03169651de28ca609ff9e544f8fb388c479c1
Evangelene Bryan <evangelenebryan@gmail.com>
2024-11-15 22:48:19 +0530

    Aura Beam Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,22 @@
+// Copyright KingdaGhost Studios
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, GetCurrentActorInfo(), CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerPlayerController()
+{
+	if (CurrentActorInfo) OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+}
